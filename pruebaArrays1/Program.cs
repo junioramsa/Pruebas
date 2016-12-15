0ad1761790390627243cf0aa9ea50becd2f01ca1
junioramsa <ajgris@gmail.com>
2016-12-15 19:58:38 +0100

    añadido un messagebox

--- pruebaArrays1/Program.cs
+++ pruebaArrays1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace pruebaArrays1
 {
@@ -11,7 +12,8 @@ namespace pruebaArrays1
         static void Main(string[] args)
         {
             string numParam;
-            Console.WriteLine("Introduzca el numero total de parámetros:");
+            Console.WriteLine("");
+            Console.Write("Introduzca el numero total de parámetros:");
             numParam = Console.ReadLine();
 
             int numParameter;
@@ -19,8 +21,11 @@ namespace pruebaArrays1
             int.TryParse(numParam, out numParameter);
             if (numParameter == 0)
             {
-                Console.WriteLine("Cero parámetros, me salgo");
-                Console.ReadLine();
+                //Al ser una aplicación de consola, si queremos que salga un cuadro de mensaje,
+                //debemos agregar una referencia de System.Windows.Forms y añadirla con using.
+                MessageBox.Show("Cero parámetros, me salgo.");
+                //Console.WriteLine("Cero parámetros, me salgo");
+                //Console.ReadLine();
             }
 
             string[] parametros = new string[numParameter];
