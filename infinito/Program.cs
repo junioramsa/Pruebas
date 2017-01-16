22841f67c45c884e6eef41fb875858d48a44cc2d
junioramsa <ajgris@gmail.com>
2017-01-16 19:39:17 +0100

    añadido loop chorra

---
+++ infinito/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace infinito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (;;) //loop infinito
+            {
+                Console.WriteLine("Infinito");
+            }
+        }
+    }
+}
